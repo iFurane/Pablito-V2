7f67a133e2c49ac485ab83e654edfcc3c9728de3
alc4m <ifurane.dev@gmail.com>
2021-09-28 20:02:28 +0530

    Initial working code

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from discord.ext import commands
 import asyncio
 import yaml
@@ -14,5 +15,12 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,
                                                         name='Los Pollos Hermanos'))
     print(f'{bot.user.name} has connected.')
+
+
+@bot.command(name='changestatus')
+async def change_status(ctx, *args):
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name=args[0]))
+    print(f'status changed')
+
 
 bot.run(TOKEN)
