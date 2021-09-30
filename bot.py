2ab9a01e411cdd39b85dbe800bcb7b7b9b6c1582
alc4m <ifurane.dev@gmail.com>
2021-09-30 21:49:18 +0530

    Removed tag functionality temporarily

--- bot.py
+++ bot.py
@@ -2,67 +2,144 @@ import discord
 from discord.ext import commands
 import datetime
 import yaml
+import os
+import shlex
+import toolbox
 
-with open(file='tokens.yml',mode='r') as f:
+# Load tokens
+with open(file='tokens.yml', mode='r') as f:
     tokens = yaml.safe_load(f)
 
-# Global variables and stores
-commands_data = {}  # Container for command data
-state_data = {}     # Container for state data, like status
+# Global variables and containers
+commands_data = {}  # Container for command data: format
+state_data = {}     # Container for state data
 colors_data = {}    # Container for color data
-channel_data = {}    # Container for Channel IDs
-reply_data={}
+channel_data = {}   # Container for Channel data
+reply_data = {}       # Container for reply formats
+roles_data = {}       # Container for roles data
+emojis_data = {}      # Container for emoji data
 boot_time = 0       # UTC time at bot's connection
 
+# Give all permissions to bot
 intents = discord.Intents.all()
+# Prefix is -
 bot = commands.Bot(command_prefix='-', intents=intents)
 
 
-#Event: bot ready
+# Event: bot connected
+@bot.event
+async def on_connect():
+    print(f'{datetime.datetime.utcnow()}: {bot.user.name} has connected.')
+
+
+# Event: bot ready
 @bot.event
 async def on_ready():
     global boot_time
     boot_time = datetime.datetime.utcnow()  # UTC time when bot connected
     update()    # Update all containers
-    print(f'{bot.user.name} has connected. \n {boot_time}') # Verify connection
+    print(f'{boot_time}: {bot.user.name} is ready.')    # Verify connection
+    await log(text=f'{bot.user.name} is ready')
 
 
 # Event: user joins
 @bot.event
 async def on_member_join(member):
+    # ask user to verify via captcha in DM
     channel = bot.get_channel((channel_data['WELCOME']['ID']))
     embed = discord.Embed(title=f'Welcome to iServer, {member}',
-                          description=f'{member.mention}, check message in your DM for verification process',
+                          description=f'{member.mention}, check message in your DM for verification process\n'
+                                      f'We have {channel.guild.member_count} members now',
                           color=colors_data['DEFAULT'])
     embed.set_thumbnail(url=member.avatar_url)
     embed.set_footer(text=f'{datetime.datetime.utcnow()}')
     await channel.send(embed=embed)
 
 
+# Verification:
+# Generate a random alphanumerical string and ask the user to type it
+# Give 3 tries within 120 seconds
+# if answer matches then give all initial roles ( Verified, member, tag recall)
+# if all tries fail then leave for manual verification
+
+
 # Event: member leaves
 @bot.event
 async def on_member_leave(member):
-    channel = bot.get_channel(channel_data['GOODBYE'])
+    channel = bot.get_channel(channel_data['GOODBYE']['ID'])
+    embed = discord.Embed(title=f'{member} has left the iServer',
+                          description=f'We have {channel.guild.member_count} members now',
+                          color=0x000000    # black,change later
+                          )
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.set_footer(text=f'{datetime.datetime.utcnow()}')
+    await channel.send(embed=embed)
+
+
+# Role management commands:
+# Role, r: give role to the author
+@bot.command(name='role')
+async def r_role(ctx, *args):
+    if ctx.channel.id != channel_data['ROLEMENU']['ID']:
+        await react_neg(ctx)
+        await ctx.reply(f'This command can only be used in <#{channel_data["ROLEMENU"]["ID"]}>', delete_after=5)
+        return
+    query = ' '.join(args).lower()
+    role = None
+    for i in roles_data['member_roles']:
+        if query == i:
+            role = ctx.guild.get_role(int(roles_data[i]['id']))
+            break
+    if role is None:
+        await react_neg(ctx)
+        return
+    await ctx.author.add_roles(role)
+    await react_pos(ctx)
+    return
+
+
+# -removerole, -remr: Removes a member role
+# Can only be used in rolemenu channel
+@bot.command(name='removerole')
+async def rem_role(ctx, *args):
+    if ctx.channel.id != channel_data['ROLEMENU']['ID']:
+        await react_neg(ctx)
+        await ctx.reply(f'This command can only be used in <#{channel_data["ROLEMENU"]["ID"]}>', delete_after=5)
+        return
+    query = ' '.join(args).lower()
+    role = None
+    for i in roles_data['member_roles']:
+        if query == i:
+            role = ctx.guild.get_role(int(roles_data[i]['id']))
+            break
+    if role is None:
+        await react_neg(ctx)
+        return
+    await ctx.author.remove_roles(role)
+    await react_pos(ctx)
+    return
 
 
 # -setstatus: Set the bot's status
-# args[0]: status type (game, listen, watch , stream)
-# if type = stream, then args[0]: url of stream
-# other args: name
+# args[0]: status type (playing, listening, watching , streaming)
+# if type = stream, then args[0]: url of stream and args[1:]: status text
 @bot.command(name='setstatus')
-async def set_status(ctx, type='', *args):
-    if type=='watch':
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=' '.join(args)))
-    elif type=='listen':
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=' '.join(args)))
-    elif type=='game':
-        await bot.change_presence(activity=discord.Game(' '.join(args)))
-    elif type=='stream':
+async def set_status(ctx, status_type='', *args):
+    status = ' '.join(args)
+    if status_type == 'watching':
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=status))
+    elif status_type == 'listening':
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=status))
+    elif status_type == 'playing':
+        await bot.change_presence(activity=discord.Game(status))
+    elif status_type == 'streaming':
         await bot.change_presence(activity=discord.Streaming(name=' '.join(args[1:]), url=args[0]))
     else:
         embed = syntax_error_embed(ctx, 'setstatus')
-        await ctx.reply(embed=embed,delete_after=reply_data['syntax_error']['timeout'])
-    print(f'status changed')
+        await ctx.reply(embed=embed, delete_after=reply_data['syntax_error']['timeout'])
+        return
+    await ctx.message.add_reaction('✅')
+    await log(f'Bot status changed')
 
 
 # -botinfo: Show information about the bot
@@ -97,12 +174,68 @@ async def server_info(ctx, *args):
     await ctx.reply(embed=embed)
 
 
+@bot.command(name='emojiinfo')
+async def emoji_info(ctx, name):
+    emojis = ctx.guild.emojis
+    await ctx.reply(f'{emojis}')
+
+
 # -channelinfo: Gives information about the given channel, or the current channel if not specified
-#@bot.command(name='channelinfo')
+# @bot.command(name='channelinfo')
 # -roleinfo: Gives information about the given role
-#@bot.command(name='roleinfo')
-# -userinfo: Gives information about the given user, or of the authir if not specified
-#@bot.command(name='userinfo')
+
+# @bot.command(name='roleinfo')
+# -memberinfo: Gives information about the given user, or of the author if not specified
+# id
+# mention
+@bot.command(name='memberinfo')
+async def member_info(ctx, *args):
+    await react_loading(ctx)
+    query = ' '.join(args)
+    if len(args) == 0:  # Nothing given: give author's info
+        member = ctx.author
+    elif query.isdigit():
+        member = ctx.guild.get_member(int(query))
+    elif query.startswith('<@!') and query.endswith('>'):
+        member = ctx.guild.get_member(int(query[3:-1]))
+    else:
+        await unreact_loading(ctx)
+        await ctx.reply('User not found')
+        return
+    if member is None:
+        await unreact_loading(ctx)
+        await ctx.reply('User either does not exist or is not a member of this server')
+        return
+    display_name = member.display_name
+    username = member
+    member_id = member.id
+    join_date = member.joined_at
+    roles = ''
+    for rl in member.roles:
+        roles += f'{rl.mention} '
+    status = member.status
+    member_since = datetime.datetime.utcnow() - join_date
+    embed = discord.Embed(title=f'{display_name}',
+                          description=f'**Username: {username}**\n'
+                                      f'**Member ID: {member_id}**\n'
+                                      f'Joined at: {join_date}\n'
+                                      f'Member since: {member_since}\n'
+                                      f'\nRoles: {roles}\n'
+                                      f'\n*Status: {status}*\n',
+                          color=member.color)
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.set_footer(text=f'{ctx.author} at {datetime.datetime.utcnow()} UTC')
+    await unreact_loading(ctx)
+    await ctx.reply(embed=embed)
+
+
+@bot.command(name='pablitohelp')
+async def pablito_help(ctx):
+    embed = discord.Embed(title=f'Pablito help',
+                          description=f'Check {bot.get_channel(channel_data["DOCS"]["ID"]).mention} for help',
+                          color=colors_data['DEFAULT'])
+    embed.set_footer(text=f'{ctx.author} at {datetime.datetime.utcnow()} UTC')
+    await ctx.reply(embed=embed,delete_after=15)
 
 
 # Create an embed for syntax errors
@@ -140,5 +273,40 @@ def update():
     with open('reply.yml','r') as fr:
         reply_data = yaml.safe_load(fr)
 
+    global roles_data
+    with open('roles.yml','r') as fro:
+        roles_data = yaml.safe_load(fro)
+
+    #global emojis_data
+    #with open('emojis.yml','r') as fe:
+    #    emojis_data = yaml.safe_load(fe)
+
+
+async def react_neg(ctx):
+    await ctx.message.add_reaction('❌')
+    return
+
+async def react_pos(ctx):
+    await ctx.message.add_reaction('✅')
+    return
+
+async def react_warn(ctx):
+    await ctx.message.add_reaction('⚠️')
+    return
+
+async def react_prohib(ctx):
+    await ctx.message.add_reaction('🚫')
+    return
+
+async def react_loading(ctx):
+    await ctx.message.add_reaction(f'<a:loading:892783955386970182>')
+    return
+
+async def unreact_loading(ctx):
+    await ctx.message.clear_reaction(f'<a:loading:892783955386970182>')
+
+async def log(text):
+    channel = bot.get_channel(channel_data['BOT_LOG']['ID'])
+    await channel.send(f'{datetime.datetime.utcnow()} UTC: {text}')
 
 bot.run(tokens['discord_token'])    # Run the bot
---
+++ toolbox/tag.py
@@ -0,0 +1,89 @@
+import yaml
+import os
+
+# Return 0 if operation successful
+# Return 1 if tag not found or already exists
+# Return 2 if other errors
+
+# Giving syntax error is command's job
+# Checking category and roles is command's job
+
+# Check for correspoding tag file
+def recall_tag(tag_name):
+    if not os.path.exists(f'../tags/{tag_name}.yml'):   # Check if file doesn't exist
+        return 1 #  File doesn't exist
+
+    tmp = {}    # Holder
+    try:
+        with open(f'../tags/{tag_name}.yml','r') as f:
+            tmp = yaml.safe_load(f)
+    except:
+        return 2    # Other error
+    return tmp  # Return the tag data, checking appropriate catgory and roles is bot's job
+
+
+# Name: name of the tag (string)
+# Author: author of the tag (id)
+# Content: data stored by tag
+# Category: category in which tag is made
+# Channel: channel in which tag is made
+# Date: date and time at tag's creation in UTC
+def tag_create(name, content, category, author, channel, date):
+    if os.path.exists(f'../tags/{name}.yml'):
+        return 1    # File (tag) exists
+    tag_dt = {'content':f'{content}', 'category':f'{category}', 'author':f'{author}', 'channel':f'{channel}','date created':f'{date}'}
+    try:
+        with open(f'../tags/{name}.yml','x') as f:
+            yaml.safe_dump(tag_dt, f)
+    except:
+        return 2    # File error
+    return 0    # Tag created successfully
+
+
+# tag_name: name of the tag being deleted
+def tag_delete(tag_name):
+    if not os.path.exists(f'../tags/{tag_name}.yml'):
+        return 1    # Tag not found
+    try:
+        os.remove(f'../tags/{tag_name}.yml')
+    except:
+        return 2    # File error
+    return 0    # Tag deleted successfully
+
+
+# Returns true if edited successfully, false if tag not found
+def edit_tag(name, new_content, m_date):
+    if not os.path.exists(f'../tags/{tag_name}.yml'):
+        return 1    # Tag not found
+    store = {}
+    try:
+        with open(f'../tags/{name}.yml','w') as f:  # Open in overwrite mode
+            store = yaml.safe_load(f)
+    except:
+        return 2    # File error
+    store['content'] = f'{new_content}'
+    if 'date modified' in store:
+        store['date modified'] = str(m_date)
+    else:
+        store.append({'date modified':f'{m_date}'})
+    return 0    # Tag edited successfully
+
+
+# Dump all tags into a list
+def list_all_tags():
+    list = os.listdir(path='../tags/')
+    tmp = {}
+    for i in list:
+        with open(f'../tags/{i}','r') as f:
+            tmp.append(yaml.safe_load(f))
+    return tmp
+
+
+# Find tags belonging to a category
+def find_tags_by_category(cat):
+    list = list_all_tags()
+    tmp = {}
+    for i in list:
+        if i['category'] == str(cat):
+            tmp.append(i)
+    return tmp
